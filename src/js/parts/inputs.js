d16bd9f390b04c43f6f1496d2c04c9990f2f40f5
mishanya <man_united_9393@mail.ru>
2020-06-23 18:25:57 +0300

    1

--- src/js/parts/inputs.js
+++
@@ -1,9 +0,0 @@
-const inputValue = () => {
-    const phoneInput = document.querySelector('input[name="phone"]');
-
-    phoneInput.addEventListener('input', () => {
-        phoneInput.value = phoneInput.value.replace(/\D/, '');
-    });
-}
-
-module.exports = inputValue;
